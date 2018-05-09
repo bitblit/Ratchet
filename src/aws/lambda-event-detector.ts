e3ca0ec3f938ab748fce8b4de02a7f2bac67b059
Chris Weiss <bitblit@gmail.com>
2018-05-09 08:58:51 -0700

    Updating lambda event detector

--- src/aws/lambda-event-detector.ts
+++ src/aws/lambda-event-detector.ts
@@ -4,18 +4,35 @@ export class LambdaEventDetector {
 
 
     public static isValidCronEvent(event: any): boolean {
-        return (event && event.source == 'aws.events' && event.resources && event.resources.length == 1);
+        return (event && event.source == 'aws.events' && event.resources && event.resources.length > 0);
     }
 
     public static isValidSnsEvent(event: any): boolean {
-        return (event && event.Records && event.Records.length == 1 && event.Records[0].EventSource == 'aws:sns');
+        return (event && event.Records && event.Records.length >0 && event.Records[0].EventSource == 'aws:sns');
     }
 
     public static isValidDynamoDBEvent(event: any): boolean {
-        return (event && event.Records && event.Records.length == 1 && event.Records[0].eventSource == 'aws:dynamodb');
+        return (event && event.Records && event.Records.length >0 && event.Records[0].eventSource == 'aws:dynamodb');
     }
 
     public static isValidS3Event(event: any): boolean {
-        return (event && event.Records && event.Records.length == 1 && event.Records[0].eventSource == 'aws:s3');
+        return (event && event.Records && event.Records.length >0 && event.Records[0].eventSource == 'aws:s3');
     }
+
+    public static isSingleCronEvent(event: any): boolean {
+        return this.isValidCronEvent(event) && event.resources.length==1;
+    }
+
+    public static isSingleSnsEvent(event: any): boolean {
+        return this.isValidSnsEvent(event) && event.Records.length==1;
+    }
+
+    public static isSingleDynamoDBEvent(event: any): boolean {
+        return this.isValidDynamoDBEvent(event) && event.Records.length==1;
+    }
+
+    public static isSingleS3Event(event: any): boolean {
+        return this.isValidS3Event(event) && event.Records.length==1;
+    }
+
 }
