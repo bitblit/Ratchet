6c7dd6c0cfa1a8b70e93f6178a5568e025a9c3ea
Chris Weiss <bitblit@gmail.com>
2018-05-17 15:59:43 -0700

    Actually adding the code

--- src/common/duration-ratchet.ts
+++ src/common/duration-ratchet.ts
@@ -19,6 +19,18 @@ export class DurationRatchet {
         return rval;
     }
 
+    public static colonFormatMsDuration(ms:number, includeMS:boolean =false): string {
+        let rem_ms = (ms % 1000);
+        let seconds = Math.floor(ms / 1000) % 60;
+        let minutes = Math.floor(ms / (1000 * 60)) % 60;
+        let hours = Math.floor(ms / (1000 * 60 * 60)) % 24;
+
+        let f = NumberRatchet.leadingZeros;
+        let rval=f(hours, 2)+":"+f(minutes,2)+":";
+        rval+=(includeMS)?f(seconds, 2)+"."+f(rem_ms,3):f(seconds, 2);
+        return rval;
+    }
+
     public static daysBetween(d1: Date, d2: Date) : number
     {
         return moment(d1).diff(moment(d2), 'days');
--- test/common/test-duration-ratchet.ts
+++ test/common/test-duration-ratchet.ts
@@ -7,3 +7,16 @@ describe('#formatMsDuration', function() {
         expect(result).to.equal('00h00m00.409s');
     });
 });
+
+describe('#colonFormatMsDuration', function() {
+    it('should format less than one second', function() {
+        let result = DurationRatchet.colonFormatMsDuration(409,true);
+        expect(result).to.equal('00:00:00.409');
+    });
+
+    it('should format more than ten hours', function() {
+        let result = DurationRatchet.colonFormatMsDuration((1000*60*60*11),false);
+        expect(result).to.equal('11:00:00');
+    });
+
+});
