c89850a81f0af3ca1d6fb3d18dea433c5367de4e
Chris Weiss <bitblit@gmail.com>
2018-06-25 16:26:30 -0700

    Adding time slots to the time zone ratchet

--- src/common/time-zone-ratchet.ts
+++ src/common/time-zone-ratchet.ts
@@ -36,4 +36,51 @@ export class TimeZoneRatchet {
         return rval;
     }
 
+    // Returns midnight in the current timezone in epoch seconds
+    public startOfTodayEpochMS() : number{
+        let startOfToday = moment().tz(this.timezone).hour(0).minute(0).second(0).millisecond(0).toDate().getTime();
+        return startOfToday;
+    }
+
+    /**
+     * Returns the number of slots in a day (simple math)
+     * @param {number} slotWidthMs
+     * @returns {number} containing the number of slots in a day (last one may be partial)
+     */
+    public dailySlotCount(slotWidthMs:number) : number{
+        return Math.ceil(86400000/slotWidthMs);
+    }
+
+    /**
+     * Imagine a day cut into N 'slots', each slotWidthMS wide - then there are
+     * 86,400,000 / slotWidthMS slots available, indexed from 0 on up.  This function
+     * returns that index
+     * @param {number} slotWidthMs
+     * @returns {number} containing the current index
+     */
+    public currentTimeSlotIdx(slotWidthMs:number) : number{
+        if (slotWidthMs<1)
+        {
+            throw new Error("Cannot process with slot less than one ms wide");
+        }
+
+        let startOfToday = this.startOfTodayEpochMS();
+        let now = new Date().getTime();
+        let delta = now-startOfToday;
+        let idx = Math.floor(delta/slotWidthMs);
+        return idx;
+    }
+
+    /**
+     * Given the definition of slotWidth and currentTimeSlotIdx from above, return the ms that is the
+     * start of the slot that "now" falls within.
+     * @param {number} slotWidthMs
+     * @returns {number}
+     */
+    public startOfCurrentSlotEpochMS(slotWidthMs:number) : number {
+        let startOfToday = this.startOfTodayEpochMS();
+        let currentIdx = this.currentTimeSlotIdx(slotWidthMs);
+        return startOfToday+(currentIdx*slotWidthMs);
+    }
+
 }
---
+++ test/common/test-time-zone-ratchet.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import {TimeZoneRatchet} from "../../src/common/time-zone-ratchet";
+import {Logger} from "../../src/common/logger";
+
+let tz: TimeZoneRatchet = TimeZoneRatchet.PACIFIC;
+
+describe('#currentHour', function() {
+    it('should return 0- 23', function() {
+        let result : number = tz.currentHour();
+        expect(result).to.be.gte(0);
+        expect(result).to.be.lte(23);
+    });
+});
+
+describe('#startOfTodayEpochSeconds', function() {
+
+    it('should never be more than now or more than 86400 seconds in the past', function() {
+        let now : number = Math.floor(new Date().getTime()/1000);
+        let start : number = tz.startOfTodayEpochSeconds();
+        let result : number = now-start;
+        expect(result).to.be.gte(0);
+        expect(result).to.be.lte(86400);
+    });
+
+});
+
+
+describe('#dailySlotCount', function() {
+
+    it('should return 86400', function() {
+        let result : number = tz.dailySlotCount(1000);
+        expect(result).to.eq(86400);
+    });
+
+});
+
+
+describe('#currentTimeSlotIdx', function() {
+
+    it('should return same as current hour', function() {
+        let curHour : number = tz.currentHour();
+        let hourSlot : number = 1000*60*60;
+        let curSlot : number = tz.currentTimeSlotIdx(hourSlot);
+
+        expect(curSlot).to.eq(curHour);
+    });
+
+});
