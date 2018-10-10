0655ed9863a55c64629901aa8bae88fa34d55585
Chris Weiss <bitblit@gmail.com>
2018-10-10 10:10:31 -0700

    Moving over the better environment service, switching to yarn

--- src/aws/environment-service.ts
+++ src/aws/environment-service.ts
@@ -1,61 +1,45 @@
 import * as AWS from 'aws-sdk';
-import {Logger} from "../common/logger";
+import {Logger} from '@bitblit/ratchet/dist/common/logger';
 
 /**
  * Service for reading environmental variables
  * Also hides the decryption detail from higher up services
  */
-
-// Reads a configuration from the system management service and caches it for use
 export class EnvironmentService {
-    private ssmEntryName : string;
-    private ssmRegion : string;
-    private ssmEncrypted : boolean;
-    private static readConfigPromise: any;
+    private static READ_CONFIG_PROMISE: Map<String,Promise<any>> = new Map();
 
-    constructor(ssmEntryName: string, ssmRegion : string = 'us-east-1', ssmEncrypted : boolean = true)
-    {
-        if (!ssmEntryName)
-        {
-            throw 'SSM Entry name cannot be null';
-        }
-        this.ssmEntryName = ssmEntryName;
-        this.ssmRegion = ssmRegion;
-        this.ssmEncrypted = ssmEncrypted;
-    }
-
-    public getConfig() : Promise<any>
-    {
-        Logger.info("EnvService:About to read config");
-        if (EnvironmentService.readConfigPromise)
-        {
-            return Promise.resolve(EnvironmentService.readConfigPromise);
-        }
-        else {
-            let ssm = new AWS.SSM({apiVersion: '2014-11-06',region: this.ssmRegion});
-            let params = {
-                Name: this.ssmEntryName, /* required */
-                WithDecryption: this.ssmEncrypted
+    public static getConfig(name:string, region:string = 'us-east-1', ssmEncrypted : boolean = true): Promise<any> {
+        Logger.info('EnvService:About to read config');
+        if (EnvironmentService.READ_CONFIG_PROMISE[name]) {
+            return Promise.resolve(EnvironmentService.READ_CONFIG_PROMISE[name]);
+        } else {
+            const ssm = new AWS.SSM({apiVersion: '2014-11-06', region: region});
+            const params = {
+                Name: name, /* required */
+                WithDecryption: ssmEncrypted
             };
 
-            return ssm.getParameter(params).promise().then(value=>{
-                Logger.info("EnvService:Finished reading config");
+            EnvironmentService.READ_CONFIG_PROMISE[name] = ssm.getParameter(params).promise().then(value => {
+                Logger.info('EnvService:Finished read config');
 
-                if (value!=null && value.Parameter!=null && value.Parameter.Value)
-                {
-                    let toParse : string = value.Parameter.Value;
+                if (value != null && value.Parameter != null && value.Parameter.Value) {
+                    const toParse: string = value.Parameter.Value;
                     return JSON.parse(toParse);
-                }
-                else
-                {
+                } else {
                     return null;
                 }
             })
-                .catch (err=>{
-                    Logger.error("Error reading environmental configuration - returning null: %s",err);
+                .catch(err => {
+                    Logger.error('Error reading environmental configuration - returning null: %s', err);
                     return null;
-                })
+                });
+
+            return EnvironmentService.READ_CONFIG_PROMISE[name];
         }
+    }
+
+    private constructor() {
+        // Private so we don't instantiate this guy
     }
 
 }
