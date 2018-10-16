a33114e42f735019eda12a161ca01a5803cfa1be
Chris Weiss <bitblit@gmail.com>
2018-10-16 13:15:51 -0700

    Lowering logging level on env service call

--- src/aws/environment-service.ts
+++ src/aws/environment-service.ts
@@ -9,12 +9,12 @@ export class EnvironmentService {
     private static READ_CONFIG_PROMISE: Map<String,Promise<any>> = new Map();
 
     public static getConfig(name:string, region:string = 'us-east-1', ssmEncrypted : boolean = true): Promise<any> {
-        Logger.debug('EnvService:About to read config');
+        Logger.silly('EnvService:Request to read config');
         if (EnvironmentService.READ_CONFIG_PROMISE.get(name)) {
-            Logger.debug('Using previous EnvService promise');
+            Logger.silly('Using previous EnvService promise');
             return Promise.resolve(EnvironmentService.READ_CONFIG_PROMISE.get(name));
         } else {
-            Logger.debug('Creating new EnvService promise');
+            Logger.silly('Creating new EnvService promise');
             const ssm = new AWS.SSM({apiVersion: '2014-11-06', region: region});
             const params = {
                 Name: name, /* required */
@@ -22,7 +22,7 @@ export class EnvironmentService {
             };
 
             EnvironmentService.READ_CONFIG_PROMISE.set(name, ssm.getParameter(params).promise().then(value => {
-                Logger.info('EnvService:Finished read config');
+                Logger.debug('EnvService:Finished read config');
 
                 if (value != null && value.Parameter != null && value.Parameter.Value) {
                     const toParse: string = value.Parameter.Value;
