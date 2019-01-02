0bee3f49a3d0fc808f3c7ada7a87cdc8bf89ea08
Chris Weiss <bitblit@gmail.com>
2019-01-02 10:43:22 -0800

    Adding circleci processor

---
+++ src/circleci/apply-circle-ci-env-variables-to-files.ts
@@ -0,0 +1,85 @@
+/*
+    Functions for working with booleans
+*/
+import * as fs from 'fs';
+import * as moment from 'moment-timezone';
+import {Logger} from '../common/logger';
+
+export class ApplyCircleCiEnvVariablesToFiles {
+
+    public static async process(fileNames: string[], timezone: string='America/Los_Angeles',
+                                                       buildFinder: string = 'LOCAL-SNAPSHOT',
+                                                       branchFinder: string = 'LOCAL-BRANCH',
+                                                       hashFinder: string = 'LOCAL-HASH',
+                                                       tagFinder: string = 'LOCAL-TAG',
+                                                       timeFinder: string = 'LOCAL-TIME'
+                                                       ): Promise<number> {
+
+        if (!fileNames) {
+            throw new Error('fileNames must be defined');
+        }
+        if (fileNames.length === 0) {
+            Logger.warn('Warning - no files supplied to process');
+        }
+        const buildNum: string = process.env['CIRCLE_BUILD_NUM'];
+        const branch: string = process.env['CIRCLE_BRANCH'] || '';
+        const tag: string = process.env['CIRCLE_TAG'] || '';
+        const sha1: string = process.env['CIRCLE_SHA1'] || '';
+        const localTime: string = moment().tz(timezone).format('MMMM Do YYYY, h:mm:ss a z');
+
+        if (!buildNum) {
+            throw new Error('CIRCLE_BUILD_NUM env var not set - apparently not in a CircleCI environment');
+        }
+
+        let foundCount: number = 0;
+        fileNames.forEach(f => {
+           if (!fs.existsSync(f)) {
+               Logger.error('Could not find file %s to process, continuing', f);
+           } else {
+               try {
+                   let contents: string = fs.readFileSync(f).toString();
+                   contents.split(buildFinder).join(buildNum);
+                   contents.split(branchFinder).join(branch);
+                   contents.split(hashFinder).join(tag);
+                   contents.split(tagFinder).join(sha1);
+                   contents.split(timeFinder).join(localTime);
+                   fs.writeFileSync(f, contents);
+                   foundCount++;
+               } catch (err) {
+                   Logger.error('Error processing %s , continuing: %s', f, err, err);
+               }
+           }
+        });
+
+        return foundCount;
+
+    }
+
+
+    public static extractFileNames(): string[] {
+        let rval: string[] = [];
+        if (process && process.argv && process.argv.length > 2) {
+            rval = process.argv.slice(2);
+        }
+        return rval;
+    }
+}
+
+
+/**
+ And, in case you are running this command line...
+ TODO: should use switches to allow setting the various non-filename params
+ **/
+Logger.info('Running ApplyCircleCiEnvVariablesToFiles from command line arguments');
+const filenames: string[] = ApplyCircleCiEnvVariablesToFiles.extractFileNames();
+if (filenames.length>0) {
+    ApplyCircleCiEnvVariablesToFiles.process(filenames).then(res => {
+        Logger.info('Processed %d files of %d', res, filenames.length);
+    })
+} else {
+    console.log('Usage : node apply-circle-ci-env-variables-to-files {file1} {file2} ...');
+}
+
+
+
+
---
+++ test/circleci/test-apply-circle-ci-env-variables-to-files.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import {BooleanRatchet} from "../../src/common/boolean-ratchet";
+import * as AWS from 'aws-sdk';
+import {S3CacheRatchet} from '../../src/aws/s3-cache-ratchet';
+import {ApplyCircleCiEnvVariablesToFiles} from '../../src/circleci/apply-circle-ci-env-variables-to-files';
+import {Logger} from '../../src/common/logger';
+
+describe('#applyCircleCiEnvVariablesToFiles', function() {
+    it('should fail if not in a circle ci environment', async() => {
+        try {
+            const result:number = await ApplyCircleCiEnvVariablesToFiles.process(['test1.txt']);
+            this.bail();
+        } catch (err) {
+            Logger.debug('Caught expected error : %s', err);
+            // Expected, return ok
+        }
+    });
+
+    it('should not fail if in a circle ci environment', async() => {
+        process.env['CIRCLE_BUILD_NUM'] = '1';
+        process.env['CIRCLE_BRANCH']='B';
+        process.env['CIRCLE_TAG']='T';
+        process.env['CIRCLE_SHA1']='S';
+
+        const result:number = await ApplyCircleCiEnvVariablesToFiles.process([]);
+        expect(result).to.eq(0);
+    });
+
+});
