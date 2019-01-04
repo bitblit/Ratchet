c8df63fdbabb82a7af6108033297a348d97e2b3e
Chris Weiss <bitblit@gmail.com>
2019-01-04 10:44:43 -0800

    Adding fixed local ip provider

---
+++ src/common/fixed-local-ip-provider.ts
@@ -0,0 +1,11 @@
+import {LocalIpProvider} from './local-ip-provider';
+
+export class FixedLocalIpProvider implements LocalIpProvider {
+
+    constructor(private fixed: string) {}
+
+    currentLocalIpAddress(): string {
+        return this.fixed;
+    }
+
+}
