13b911eec572fbd6f7b07d397a8b19a463401dd5
Chris Weiss <bitblit@gmail.com>
2019-07-12 16:44:16 -0700

    Adding mailer

--- src/aws/ses/mailer.ts
+++ src/aws/ses/mailer.ts
@@ -3,7 +3,7 @@ import {RemoteHandlebarsTemplateRenderer} from './remote-handlebars-template-ren
 import {ReadyToSendEmail} from './ready-to-send-email';
 import {RequireRatchet} from '../../common/require-ratchet';
 import {Logger} from '../../common/logger';
-import {SendEmailRequest, SendEmailResponse} from 'aws-sdk/clients/ses';
+import {SendEmailRequest, SendEmailResponse, SendRawEmailRequest, SendRawEmailResponse} from 'aws-sdk/clients/ses';
 
 export class Mailer {
     public static readonly EMAIL: RegExp = new RegExp(".+@.+\\.[a-z]+");
@@ -21,10 +21,65 @@ export class Mailer {
         return rts;
     }
 
-    public async sendEmail(rts:ReadyToSendEmail): Promise<SendEmailResponse> {
+    public async sendEmail(rts:ReadyToSendEmail): Promise<SendRawEmailResponse> {
+        let rval: SendRawEmailResponse = null;
+        try {
+            const from: string = rts.fromAddress || this.defaultSendingAddress;
+            const boundary: string = 'NextPart';
+            let rawMail:string = 'From: '+from+'\n';
+            rawMail += 'To: ' + rts.destinationAddresses.join(', ') + '\n';
+            if (!!this.autoBccAddresses && this.autoBccAddresses.length>0) {
+                rawMail +='Bcc: ' + this.autoBccAddresses.join(', ') + '\n';
+            }
+            rawMail += 'Subject: '+rts.subject+'\n';
+            rawMail += 'MIME-Version: 1.0\n';
+            rawMail += 'Content-Type: multipart/mixed; boundary="'+boundary+'"\n';
+            if (!!rts.htmlMessage) {
+                rawMail += '\n\n--'+boundary+'\n';
+                rawMail += 'Content-Type: text/html\n\n';
+                rawMail += rts.htmlMessage;
+            }
+            if (!!rts.txtMessage) {
+                rawMail += '\n\n--'+boundary+'\n';
+                rawMail += 'Content-Type: text/plain\n\n';
+                rawMail += rts.txtMessage;
+            }
+            if (rts.attachments) {
+                rts.attachments.forEach(a=>{
+                    rawMail += '\n\n--'+boundary+'\n';
+                    rawMail += 'Content-Type: '+a.contentType+'; name="'+a.filename+'"\n';
+                    rawMail += 'Content-Transfer-Encoding: base64\n';
+                    rawMail += 'Content-Disposition: attachment\n\n';
+                    rawMail += a.base64Data.replace(/([^\0]{76})/g, '$1\n') + '\n\n';
+                })
+            }
+            rawMail += '\n\n--'+boundary+'\n';
+
+            const params: SendRawEmailRequest = {
+                RawMessage: {Data: rawMail}
+            };
+
+            rval = await this.ses.sendRawEmail(params).promise();
+        } catch (err) {
+            Logger.error("Error while processing email: %s" ,err,err);
+        }
+
+        return rval;
+
+    }
+
+    public static validEmail(email:string):boolean {
+        return email !== null && Mailer.EMAIL.test(email);
+    }
+
+
+    /*
+    public async sendEmail2(rts:ReadyToSendEmail): Promise<SendEmailResponse> {
         let rval: SendEmailResponse = null;
 
         try {
+            //const p: SendRawEmailRequest = {};
+
             const params: SendEmailRequest = {
                 Destination: {
                     ToAddresses: rts.destinationAddresses,
@@ -46,25 +101,14 @@ export class Mailer {
                 Source: rts.fromAddress || this.defaultSendingAddress
             };
 
-            /*
-            if (rts.getAttachments() != null) {
-                for (EmailAttachment a : rts.getAttachments()) {
-                    msg.addAttachment(a.getFilename(), new ByteArrayResource(a.getData()), a.getContentType());
-                }
-            }
-            */
-
             rval = await this.ses.sendEmail(params).promise();
 
             Logger.debug('Got send result : %j', rval);
         } catch (err) {
-            Logger.error("Error while processing the error email thread: %s" ,err,err);
+            Logger.error("Error while processing email: %s" ,err,err);
         }
         return rval;
     }
-
-    public static validEmail(email:string):boolean {
-        return email !== null && Mailer.EMAIL.test(email);
-    }
+    */
 
 }
--- test/aws/test-mailer.ts
+++ test/aws/test-mailer.ts
@@ -5,24 +5,43 @@ import {S3CacheRatchet} from '../../src/aws/s3-cache-ratchet';
 import {ReadyToSendEmail} from '../../src/aws/ses/ready-to-send-email';
 import {SendEmailResponse} from 'aws-sdk/clients/ses';
 import {Mailer} from '../../src/aws/ses/mailer';
+import {EmailAttachment} from '../../src/aws/ses/email-attachment';
+import {StringRatchet} from '../../src/common/string-ratchet';
+import {Base64Ratchet} from '../../src/common/base64-ratchet';
+import * as fs from 'fs';
 
 describe('#mailer', function() {
     it('should send email', async() => {
-        //this.bail();
+        this.bail();
+        /*
         let ses: AWS.SES = new AWS.SES({region: 'us-east-1'});
-        let svc: Mailer = new Mailer(ses);
+        let svc: Mailer = new Mailer(ses, 'test1@test.com', ['test2@test.com','test2@test.com']);
+
+        const attach1: EmailAttachment = {
+          filename: 'test.txt',
+          contentType: 'text/plain',
+          base64Data: Base64Ratchet.generateBase64VersionOfString('This is a test2')
+        };
+
+        const attach2: EmailAttachment = {
+            filename: 'a2.jpg',
+            contentType: 'image/jpg',
+            base64Data: fs.readFileSync('a2.jpg').toString('base64')
+        };
 
         let rts: ReadyToSendEmail = {
             txtMessage: 'test txt',
             htmlMessage: '<h1>Test html</h1><p>Test paragraph</p>',
             subject: 'Test subject',
             fromAddress: 'test@adomni.com',
-            destinationAddresses: ['cweiss@adomni.com']
+            destinationAddresses: ['test4@test.com'],
+            attachments: [attach1,attach2]
         }
 
         const result: SendEmailResponse = await svc.sendEmail(rts);
 
         expect(result).to.not.equal(null);
+        */
     });
 
 });
