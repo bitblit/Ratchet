45777000321a2538b97c5f6a2743baca241a1624
Chris Weiss <bitblit@gmail.com>
2019-07-26 09:41:01 -0700

    Adding no-op, fix observable test, rev version because observableratchet not backwards compatible

---
+++ src/common/no.ts
@@ -0,0 +1,9 @@
+
+// Clean way to do a no-op
+export class No {
+
+    public static op(...args: any[]): void {
+
+    }
+
+}
---
+++ test/common/test-no.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import {TransformRatchet} from "../../src/common/transform-ratchet";
+import {BuiltInTransforms} from "../../src/common/transform/built-in-transforms";
+import {PromiseRatchet} from '../../src/common/promise-ratchet';
+import {No} from '../../src/common/no';
+
+describe('#no', function() {
+    it('should do nothing', ()=> {
+        PromiseRatchet.wait(100).then(No.op);
+    });
+
+});
