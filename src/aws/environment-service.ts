d9031da043b97bd1fb641efaf35cb0a97aa8308a
Chris Weiss <bitblit@gmail.com>
2019-12-14 23:31:35 -0800

    Adding s3 based environment service support

--- src/aws/environment-service.ts
+++ src/aws/environment-service.ts
@@ -4,6 +4,11 @@ import {GetParameterResult} from 'aws-sdk/clients/ssm';
 import {AWSError} from 'aws-sdk';
 import {PromiseResult} from 'aws-sdk/lib/request';
 import {PromiseRatchet} from '../common/promise-ratchet';
+import {RequireRatchet} from '../common/require-ratchet';
+import {ClientConfiguration} from 'aws-sdk/clients/s3';
+import {S3CacheRatchet} from './s3-cache-ratchet';
+import {ErrorRatchet} from '../common/error-ratchet';
+import {StopWatch} from '../common/stop-watch';
 
 /**
  * Service for reading environmental variables
@@ -22,12 +27,62 @@ export class EnvironmentService {
             Logger.silly('Using previous EnvService promise');
         } else {
             Logger.debug('Created new EnvService promise (for %s) and registered, returning', name);
-            EnvironmentService.READ_CONFIG_PROMISE.set(name, this.retryingGetParams(name, region, ssmEncrypted, 4, 2000));
+            EnvironmentService.READ_CONFIG_PROMISE.set(name, this.retryingGetParamsSSM(name, region, ssmEncrypted, 4, 2000));
         }
         return EnvironmentService.READ_CONFIG_PROMISE.get(name);
     }
 
-    private static async retryingGetParams(name: string, region: string = 'us-east-1', ssmEncrypted: boolean = true,
+    // Alternative method that fetches config from S3 instead
+    public static async getConfigS3(bucketName: string, path: string, region: string = 'us-east-1'): Promise<any> {
+        RequireRatchet.notNullOrUndefined(bucketName);
+        RequireRatchet.notNullOrUndefined(path);
+        const storeKey: string = 's3://' + bucketName + '/' + path;
+        Logger.silly('EnvService:Request to read config from : %s', storeKey);
+        if (EnvironmentService.READ_CONFIG_PROMISE.has(storeKey)) {
+            Logger.silly('Using previous EnvService promise');
+        } else {
+            Logger.debug('Created new EnvService promise (for %s) and registered, returning', storeKey);
+            EnvironmentService.READ_CONFIG_PROMISE.set(storeKey, this.retryingGetParamsS3(bucketName, path, region, 4, 2000));
+        }
+        return EnvironmentService.READ_CONFIG_PROMISE.get(storeKey);
+    }
+
+    private static async retryingGetParamsS3(bucketName: string, path: string, region: string = 'us-east-1',
+                                              maxRetries: number, backoffMultiplierMS: number):Promise<any> {
+        Logger.silly('Creating new EnvService promise for s3 : %s / %s', bucketName, path);
+        const sw: StopWatch = new StopWatch();
+        sw.start();
+        const s3: AWS.S3 = new AWS.S3({region: region} as ClientConfiguration);
+        const ratchet: S3CacheRatchet = new S3CacheRatchet(s3, bucketName);
+
+        let tryCount: number = 0;
+        let parsedValue: any = null;
+
+        while (!parsedValue && tryCount<maxRetries) {
+            tryCount++;
+            try {
+                parsedValue = await ratchet.readCacheFileToObject<any>(path);
+            } catch (err) {
+                const errCode: string = err.code || '';
+                // TODO: Recoverable errors would go here
+                Logger.error('Final environment fetch error (code: %s) (cannot retry) : %s',errCode,err,err);
+                    throw err;
+            }
+        }
+
+        // If we reach here with a string result, try to parse it
+        if (!!parsedValue) {
+            sw.stop();
+            Logger.debug('Loaded params : %s', sw.dump());
+            return parsedValue;
+        } else {
+            ErrorRatchet.throwFormattedErr('Could not find system parameter in s3 at %s / %s in this account',
+                bucketName, path);
+        }
+
+    }
+
+    private static async retryingGetParamsSSM(name: string, region: string = 'us-east-1', ssmEncrypted: boolean = true,
                                            maxRetries: number, backoffMultiplierMS: number):Promise<any> {
         Logger.silly('Creating new EnvService promise for %s', name);
         const ssm = new AWS.SSM({apiVersion: '2014-11-06', region: region});
--- test/aws/test-environment-service.ts
+++ test/aws/test-environment-service.ts
@@ -7,8 +7,7 @@ import {Logger} from '../../src/common/logger';
 
 describe('#environmentService', function() {
     this.timeout(30000);
-    /*
-    it('should throw exception on missing environment values', async() => {
+    xit('should throw exception on missing environment values', async() => {
         try {
             const vals: any = await EnvironmentService.getConfig('i_do_not_exist');
             this.bail();
@@ -19,14 +18,22 @@ describe('#environmentService', function() {
         }
 
     });
-    */
 
-    /*
-    it('should find a valid value', async() => {
+    xit('should find a valid value', async() => {
         const vals: any = await EnvironmentService.getConfig('xxx', 'us-east-1', true);
         expect(vals).to.not.be.null;
     });
 
-     */
+    xit('should load config from s3', async() => {
+        Logger.setLevelByName('silly');
+        const bucket: string = 'xxx';
+        const path: string = 'yyy';
+
+        const vals: any = await EnvironmentService.getConfigS3(bucket,path, 'us-east-1');
+        const vals1: any = await EnvironmentService.getConfigS3(bucket,path, 'us-east-1');
+        const vals2: any = await EnvironmentService.getConfigS3(bucket,path, 'us-east-1');
+        expect(vals).to.not.be.null;
+    });
+
 
 });
