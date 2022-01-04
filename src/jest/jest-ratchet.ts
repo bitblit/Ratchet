2cc7f8c295113800e47009400cf2842e2d8b4764
Christopher Weiss <bitblit@gmail.com>
2022-01-04 10:25:37 -0800

    Adding jest ratchet

---
+++ src/jest/jest-ratchet.ts
@@ -0,0 +1,18 @@
+/*
+    Functions for working with geolocations
+*/
+
+export class JestRatchet {
+  // Empty constructor prevents instantiation
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+
+  public static mock<T>(): jest.Mocked<T> {
+    const get = function (_, key) {
+      if (!this[key]) this[key] = jest.fn();
+      return this[key];
+    };
+    const proxy = new Proxy({}, { get });
+    return proxy as unknown as jest.Mocked<T>;
+  }
+}
