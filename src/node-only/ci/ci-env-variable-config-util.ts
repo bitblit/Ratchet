6c52811266a2045c33f1df446bb776f6a14462dc
Christopher Weiss <bitblit@gmail.com>
2022-01-25 23:58:29 -0800

    Fix date, general release

--- src/node-only/ci/ci-env-variable-config-util.ts
+++ src/node-only/ci/ci-env-variable-config-util.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 import { CiEnvVariableConfig } from './ci-env-variable-config';
 
 export class CiEnvVariableConfigUtil {
-  public static readonly DEFAULT_TIME_FORMAT: string = 'MMMM Do yyyy, h:mm:ss a z';
+  public static readonly DEFAULT_TIME_FORMAT: string = 'yyyy-MM-dd HH:mm:ss a z';
   public static readonly DEFAULT_TIME_ZONE: string = 'America/Los_Angeles';
 
   public static createDefaultCircleCiVariableConfig(timezone = CiEnvVariableConfigUtil.DEFAULT_TIME_ZONE): CiEnvVariableConfig {
