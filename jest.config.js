bb33739ca34b8bf386b571f1c2d2667524357183
Christopher Weiss <bitblit@gmail.com>
2022-11-01 02:20:47 -0600

    Adding coverage, artifact upload

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'artifacts/coverage',
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      lines: 10,
+    },
+  },
 };
