3bbb2ce63016bbad056031898d01b7b926a1a196
Christopher Weiss <bitblit@gmail.com>
2023-03-11 17:58:26 -0800

    Refactor

--- modules/aws/src/build/ratchet-aws-info.ts
+++ modules/aws/src/build/ratchet-aws-info.ts
@@ -1,6 +1,6 @@
 import { BuildInformation } from '@bitblit/ratchet-common';
 
-export class RatchetInfo {
+export class RatchetAwsInfo {
   // Empty constructor prevents instantiation
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
