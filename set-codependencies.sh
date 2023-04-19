ea02a2a24cb295d611e560b9ea49caaca221c1d9
Christopher Weiss <bitblit@gmail.com>
2023-04-18 22:20:11 -0700

    Checkpoint

--- set-codependencies.sh
+++ set-codependencies.sh
@@ -2,8 +2,8 @@ if [ $# -eq 0 ]; then
     >&2 echo "No arguments provided"
     exit 1
 fi
-echo "Applying version number to all package.json files"
-find . -regex "\./modules/[^/]*/package.json" -type f -exec sed -i -e 's/"*"/$1/g' {} \;
+echo "Applying version number to all package.json files: $1"
+find . -regex "\./modules/[^/]*/package.json" -type f -exec sed -i -e "s/""*""/$1/g" {} \;
 
 
 
