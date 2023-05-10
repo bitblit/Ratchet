ef81e3e9c1930abfea885e87bd0342c6cae8a273
Christopher Weiss <bitblit@gmail.com>
2023-05-10 09:24:47 -0700

    Checkpoint

---
+++ modules/aws/esbuild-conf.mjs
@@ -0,0 +1,32 @@
+import * as esbuild from 'esbuild'
+
+import { polyfillNode } from "esbuild-plugin-polyfill-node";
+import { globPlugin } from 'esbuild-plugin-glob';
+
+const sharedConfig = {
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    plugins: [globPlugin(),polyfillNode()],
+};
+
+/*
+Not building... for now
+await esbuild.build({
+    ...sharedConfig,
+    splitting: false, // for now
+    platform: 'node', // for CJS
+    outfile: "lib/index.cjs",
+});
+
+ */
+
+await esbuild.build({
+    ...sharedConfig,
+    splitting: false, // for now
+    outfile: "lib/index.mjs",
+    platform: 'browser', // for ESM
+    format: "esm",
+});
+
