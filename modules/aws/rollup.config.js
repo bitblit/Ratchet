8ca046fa360d5c719c801ae2a2c95ac8e74d718e
Christopher Weiss <bitblit@gmail.com>
2023-05-11 23:56:01 -0700

    Checkpoint --skipTests

--- modules/aws/rollup.config.js
+++ modules/aws/rollup.config.js
@@ -5,7 +5,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-//import nodePolyfills from 'rollup-plugin-polyfill-node';
+//import nodePolyfills from 'rollup-plugin-polyfill-node';;
 
 export default {
   input: './src/index.ts',
