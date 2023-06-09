daeea2ff879826775dd15212a091a34c386a465d
Christopher Weiss <bitblit@gmail.com>
2023-06-08 20:28:19 -0700

    Tweak perms

--- apply-github-info.sh
+++ apply-github-info.sh
@@ -1,7 +1,8 @@
 echo "Applying Github variables to build files"
 echo "This script is just to handle the chicken-and-egg problem with apply-ci-env-variables-to-files for Ratchet"
 echo "no one else should use it"
-CURRENT_DATE=`date +"%Y-%m-%dT%H:%M:%S%z"` && find . -wholename "*/lib/*/build/*.js" -type f -exec sed -i -e "s/LOCAL-TIME-ISO/$CURRENT_DATE/g" \
+#CURRENT_DATE=`date +"%Y-%m-%dT%H:%M:%S%z"` && find . -wholename "*/lib/*/build/*.js" -type f -exec sed -i -e "s/LOCAL-TIME-ISO/$CURRENT_DATE/g" \
+CURRENT_DATE=`date +"%Y-%m-%dT%H:%M:%S%z"` && find . -wholename "*/lib/index.mjs" -type f -exec sed -i -e "s/LOCAL-TIME-ISO/$CURRENT_DATE/g" \
 -e "s/LOCAL-SNAPSHOT/$GITHUB_RUN_NUMBER/g" \
 -e "s/LOCAL-BRANCH/$GITHUB_REF_NAME/g" \
 -e "s/LOCAL-HASH/$GITHUB_SHA/g" \
