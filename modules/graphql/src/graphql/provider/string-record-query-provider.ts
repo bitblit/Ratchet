b95a730b265a289b6cfd86da87e25199f492ea94
Christopher Weiss <bitblit@gmail.com>
2023-11-02 12:13:46 -0700

    Add ability to intake folders

---
+++ modules/graphql/src/graphql/provider/string-record-query-provider.ts
@@ -0,0 +1,36 @@
+import { GraphqlRatchetQueryProvider } from './graphql-ratchet-query-provider.js';
+import { Logger, StringRatchet } from '@bitblit/ratchet-common';
+
+export class StringRecordQueryProvider implements GraphqlRatchetQueryProvider {
+  private files: Record<string, string> = {};
+  constructor(
+    inFiles: Record<string, string>,
+    private autoPrefix: string = '',
+    private autoSuffix: string = '',
+    private caseSensitive: boolean,
+  ) {
+    if (inFiles) {
+      if (caseSensitive) {
+        this.files = Object.assign({}, inFiles);
+      } else {
+        Object.keys(inFiles).forEach((k) => {
+          this.files[k.toLowerCase()] = inFiles[k];
+        });
+        this.files = {};
+      }
+    }
+  }
+
+  public async fetchQueryText(qry: string): Promise<string> {
+    let lookupKey: string = '';
+    lookupKey += this.caseSensitive ? StringRatchet.trimToEmpty(this.autoPrefix) : StringRatchet.trimToEmpty(this.autoPrefix).toLowerCase();
+    lookupKey += this.caseSensitive ? StringRatchet.trimToEmpty(qry) : StringRatchet.trimToEmpty(qry).toLowerCase();
+    lookupKey += this.caseSensitive ? StringRatchet.trimToEmpty(this.autoSuffix) : StringRatchet.trimToEmpty(this.autoSuffix).toLowerCase();
+
+    const rval: string = this.files[lookupKey];
+    if (!rval) {
+      Logger.warn('Could not find %s in %j', lookupKey, Object.keys(this.files));
+    }
+    return rval;
+  }
+}
