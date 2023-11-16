4c12b38fe00a3337a52eb15ee7d864d35bf84a97
Christopher Weiss <bitblit@gmail.com>
2023-11-16 00:28:00 -0800

    Add type guard

--- src/aws/lambda/lambda-event-type-guards.ts
+++ src/aws/lambda/lambda-event-type-guards.ts
@@ -1,11 +1,15 @@
 // Simple switch to see what kind of event I am looking at
 /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
-import { APIGatewayEvent, APIGatewayProxyEventV2, DynamoDBStreamEvent, S3Event, ScheduledEvent, SNSEvent } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyEventV2, DynamoDBStreamEvent, S3Event, ScheduledEvent, SNSEvent, SQSEvent } from 'aws-lambda';
 import { LambdaEventDetector } from './lambda-event-detector';
 
 export class LambdaEventTypeGuards {
   public static isValidCronEvent(event: any): event is ScheduledEvent {
     return LambdaEventDetector.isValidCronEvent(event);
+  }
+
+  public static isValidSqsEvent(event: any): event is SQSEvent {
+    return LambdaEventDetector.isValidSqsEvent(event);
   }
 
   public static isValidSnsEvent(event: any): event is SNSEvent {
