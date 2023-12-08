bc16679d71a9735e6ebc1fe7c66fbb1947b19e1e
Christopher Weiss <bitblit@gmail.com>
2023-12-08 12:47:11 -0800

    Update libs

--- modules/warden-server/rollup.config.js
+++ modules/warden-server/rollup.config.js
@@ -1,11 +1,8 @@
 // rollup.config.js
 
 import typescript from '@rollup/plugin-typescript';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
+
 export default {
   input: './src/index.ts',
   output: {
