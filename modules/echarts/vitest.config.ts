7d48f205f618bedc30adf2fa4f42ac6f688c9312
Christopher Weiss <bitblit@gmail.com>
2024-04-11 23:32:18 -0700

    Move to vite

--- modules/echarts/vitest.config.ts
+++ modules/echarts/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    pool: 'forks',
     coverage: {
       reportsDirectory: 'artifacts/coverage',
       provider: 'istanbul', // or 'v8'
