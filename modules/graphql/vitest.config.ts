f46398da80dda053b8e560e7daba935385f16195
Christopher Weiss <bitblit@gmail.com>
2024-04-11 23:37:47 -0700

    Move to vite

--- modules/graphql/vitest.config.ts
+++ modules/graphql/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     pool: 'forks',
+    passWithNoTests: true,
     coverage: {
       reportsDirectory: 'artifacts/coverage',
       provider: 'istanbul', // or 'v8'
