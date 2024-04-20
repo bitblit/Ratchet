4a9cbce7e86bc1a61c2ee609fb5069ba34425c62
Christopher Weiss <bitblit@gmail.com>
2024-04-19 17:19:58 -0700

    Another sobol attempt

---
+++ modules/misc/src/sobol/generated/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgreementsApi.js';
+export * from './CustomFieldValuesApi.js';
+export * from './CustomFieldsApi.js';
+export * from './GoalsApi.js';
+export * from './RoleAssignmentsApi.js';
+export * from './RolesApi.js';
+export * from './TeamsApi.js';
+export * from './UsersApi.js';
