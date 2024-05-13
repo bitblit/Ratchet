7b24f210d77b3bafbf3ab4eb103e97300936178a
Christopher Weiss <bitblit@gmail.com>
2024-05-12 23:11:35 -0700

    Debugging vpc

--- modules/epsilon-deployment/src/deployment/cdk/epsilon-api-stack-props.ts
+++ modules/epsilon-deployment/src/deployment/cdk/epsilon-api-stack-props.ts
@@ -1,6 +1,7 @@
 import { StackProps } from 'aws-cdk-lib';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { EpsilonApiStackFeature } from './epsilon-api-stack-feature.js';
+import { SubnetAttributes } from "aws-cdk-lib/aws-ec2";
 
 export interface EpsilonApiStackProps extends StackProps {
   batchInstancesEc2KeyPairName?: string;
@@ -12,8 +13,8 @@ export interface EpsilonApiStackProps extends StackProps {
   dockerFileName: string;
 
   lambdaSecurityGroupIds: string[];
-  vpcPrivateSubnetIds: string[];
-  vpcPublicSubnetIds: string[];
+  vpcPrivateSubnets: SubnetAttributes[];
+  vpcPublicSubnets: SubnetAttributes[];
   availabilityZones: string[];
   vpcId: string;
 
--- modules/epsilon-deployment/src/deployment/cdk/epsilon-api-stack.ts
+++ modules/epsilon-deployment/src/deployment/cdk/epsilon-api-stack.ts
@@ -55,10 +55,10 @@ export class EpsilonApiStack extends Stack {
 
      */
     const sharedVpcPrivateSubnetSelection: SubnetSelection = {
-      subnets: props.vpcPrivateSubnetIds.map((subnetId, index) => Subnet.fromSubnetId(this, `VpcPrivateSubnet${index}`, `subnet-${subnetId}`)),
+      subnets: props.vpcPrivateSubnets.map((subnet, index) => Subnet.fromSubnetAttributes(this, `VpcPrivateSubnet${index}`, subnet)),
     };
     const sharedVpcPublicSubnetSelection: SubnetSelection = {
-      subnets: props.vpcPublicSubnetIds.map((subnetId, index) => Subnet.fromSubnetId(this, `VpcPublicSubnet${index}`, `subnet-${subnetId}`)),
+      subnets: props.vpcPublicSubnets.map((subnet, index) => Subnet.fromSubnetAttributes(this, `VpcPublicSubnet${index}`, subnet)),
     };
     const fargateVpcSecurityGroups: ISecurityGroup[] = props.lambdaSecurityGroupIds.map((sgId, index) =>
       SecurityGroup.fromSecurityGroupId(this, `SecurityGroup${index}`, `sg-${sgId}`),
