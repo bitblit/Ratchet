f6abacfe9e90eef5191f364fa3a1812722aac005
Christopher Weiss <bitblit@gmail.com>
2024-07-28 14:30:56 -0700

    Try to get tests to run

---
+++ jest-setup.js
@@ -0,0 +1,2 @@
+// Using require here, use import in the new ESM version
+require('reflect-metadata');
--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'artifacts/coverage',
+  setupFiles: ['./jest-setup.js'],
   /*
-  This is recommended to catch files you arent testing at all, but will
+  This is recommended to catch files you are not testing at all, but will
   need tweaks since I don't want to test index files, etc.  A job
   for a different day
   collectCoverageFrom: ['./src/**'],
