445c2baa991c8ee2639335bf6120a828f9957721
Christopher Weiss <bitblit@gmail.com>
2024-08-16 22:38:49 -0700

    First shot at angular libs

--- set-codependencies.sh
+++ set-codependencies.sh
@@ -4,6 +4,7 @@ if [ $# -eq 0 ]; then
 fi
 echo "Applying version number to all package.json files: $1"
 find . -regex "\./modules/[^/]*/package.json" -type f -exec sed -i -e "s/0.0.0-snapshot/$1/g" {} \;
+find . -regex "\./angular-modules/[^/]*/package.json" -type f -exec sed -i -e "s/0.0.0-snapshot/$1/g" {} \;
 
 
 
