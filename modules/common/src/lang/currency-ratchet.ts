3d1361eba2f5fbbc04d91c1a09a2fb5dd5eed777
Christopher Weiss <bitblit@gmail.com>
2024-09-18 21:13:46 -0700

    Tweak pct format

--- modules/common/src/lang/currency-ratchet.ts
+++ modules/common/src/lang/currency-ratchet.ts
@@ -5,23 +5,16 @@ export class CurrencyRatchet {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
 
-  public static pctFormatted(pct: number): string {
-    const rval: string =
-      pct === null || pct === undefined
-        ? 'Null'
-        : pct.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '%';
-    return rval;
-  }
 
-  public static centToDollarFormat(cents: number): string {
+  public static centToDollarFormat(cents: number, fractionDigits: number = 2): string {
     return CurrencyRatchet.dollarFormat(cents / 100);
   }
 
-  public static dollarFormat(dollars: number): string {
+  public static dollarFormat(dollars: number, fractionDigits: number = 2): string {
     const rval: string =
       dollars === null || dollars === undefined
         ? 'Null'
-        : '$' + dollars.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        : '$' + dollars.toLocaleString('en-US', { minimumFractionDigits: fractionDigits, maximumFractionDigits: fractionDigits });
     return rval;
   }
 
@@ -31,16 +24,6 @@ export class CurrencyRatchet {
       rval = range.low ? CurrencyRatchet.dollarFormat(range.low) : ' ^ ';
       rval += ' - ';
       rval += range.high ? CurrencyRatchet.dollarFormat(range.high) : ' ^ ';
-    }
-    return rval;
-  }
-
-  public static percentFormatNumberRange(range: Range<number>): string {
-    let rval: string = 'N/A';
-    if (range) {
-      rval = range.low ? CurrencyRatchet.pctFormatted(range.low) : ' ^ ';
-      rval += ' - ';
-      rval += range.high ? CurrencyRatchet.pctFormatted(range.high) : ' ^ ';
     }
     return rval;
   }
