20647361ca060a477af001f506288eead49ab702
Christopher Weiss <bitblit@gmail.com>
2024-10-17 21:56:17 -0700

    Backport

---
+++ modules/node-only/src/third-party/angular/angular-aot-rollup-plugin.ts
@@ -0,0 +1,19 @@
+import { execSync } from 'child_process';
+import path from 'path';
+
+// Custom Rollup plugin to run Angular AOT
+export default function angularAotPlugin() {
+  return {
+    name: 'angular-aot-rollup-plugin',
+    buildStart() {
+      console.log('Running Angular AOT Compilation...');
+      try {
+        execSync('ngc -p tsconfig.json', { stdio: 'inherit' });
+      } catch (error) {
+        this.error('AOT Compilation failed.');
+      }
+    }
+  };
+}
+
+//module.exports = angularAotPlugin;
