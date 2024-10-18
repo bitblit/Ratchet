fa2cdce3e1a0866210f03a971d2f9cba61eb85a4
Christopher Weiss <bitblit@gmail.com>
2024-10-17 17:06:15 -0700

    Update index

--- modules/acute-common/src/index.ts
+++ modules/acute-common/src/index.ts
@@ -1,2 +1,22 @@
 export * from './build/ngx-acute-common-info';
-export * from './components/dialogs/alert/alert.component'
+
+export * from './components/dialogs/alert/alert.component';
+export * from './components/dialogs/block-ui/block-ui.component';
+
+export * from './model/google-analytics-config';
+
+export * from './pipes/capitalize.pipe';
+export * from './pipes/dollar-formatted.pipe';
+export * from './pipes/map-values.pipe';
+export * from './pipes/number-with-commas.pipe';
+export * from './pipes/order-by.pipe';
+export * from './pipes/percent-formatted.pipe';
+export * from './pipes/plural.pipe';
+export * from './pipes/round.pipe';
+export * from './pipes/time-ago-formatted.pipe';
+export * from './pipes/timing.pipe';
+
+export * from './services/google-analytics.service';
+export * from './services/graphql-query.service';
+export * from './services/local-storage.service';
+export * from './services/window-ref.service';
