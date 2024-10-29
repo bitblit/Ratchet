623c2fc6a955e97721437503b12e30f590a94162
Christopher Weiss <bitblit@gmail.com>
2024-10-29 12:39:36 -0700

    Checkpoint - fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,18 +6,16 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
-//  ...tseslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   eslintConfigPrettier,
   {
     rules: {
       // Ok, I'm a java-head, but I like the namespacing of static class wrappers
-      "typescript-eslint/no-extraneous-class": "off",
-      "typescript-eslint/no-inferrable-types": "off",
-      "typescript-eslint/no-explicit-any": "off",
-      "no-unused-vars": "off",
-      "typescript-eslint/no-unused-vars": ["error", {
+      "@typescript-eslint/no-extraneous-class": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["error", {
         "argsIgnorePattern": "^_",
         "varsIgnorePattern": "^_"
       }],
