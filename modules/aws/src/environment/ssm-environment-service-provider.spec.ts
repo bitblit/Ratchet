eea38cab601cf41b16895089b2fa545f05da2571
Christopher Weiss <bitblit@gmail.com>
2025-01-22 14:27:39 -0800

    Fix test

---
+++ modules/aws/src/environment/ssm-environment-service-provider.spec.ts
@@ -0,0 +1,20 @@
+import { EnvironmentService } from "./environment-service.js";
+import { describe, expect, test } from "vitest";
+import { SsmEnvironmentServiceProvider } from "./ssm-environment-service-provider.js";
+import { AwsCredentialsRatchet } from "../iam/aws-credentials-ratchet";
+
+
+describe('#ssmEnvironmentService', function () {
+  test.skip('should throw exception on missing environment values', async () => {
+    const profile: string = 'tmp'; // erigir
+    const config: string='cfg';//BellaLinguaConfiguration
+      AwsCredentialsRatchet.applySetProfileEnvironmentalVariable(profile);
+      const environmentService: EnvironmentService<any> = new EnvironmentService<any>(new SsmEnvironmentServiceProvider(), {
+        maxRetries: 5,
+        backoffMultiplierMS: 500,
+      });
+      const cfg: any = await environmentService.getConfig(config);
+      expect(cfg).not.toBeNull;
+  });
+
+});
