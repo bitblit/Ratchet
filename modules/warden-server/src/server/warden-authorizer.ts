a602ba094b03f41c2b217ffa4bcf1cc7a8997be4
Christopher Weiss <bitblit@gmail.com>
2025-09-02 13:33:41 -0700

    Adding cacheable

--- modules/warden-server/src/server/warden-authorizer.ts
+++ modules/warden-server/src/server/warden-authorizer.ts
@@ -3,7 +3,6 @@ import { WardenContact } from "@bitblit/ratchet-warden-common/common/model/warde
 import { WardenEntry } from "@bitblit/ratchet-warden-common/common/model/warden-entry";
 import { WardenUtils } from "@bitblit/ratchet-warden-common/common/util/warden-utils";
 import { WardenUserDecoration } from "@bitblit/ratchet-warden-common/common/model/warden-user-decoration";
-import { StringRatchet } from "@bitblit/ratchet-common/lang/string-ratchet";
 
 export class WardenAuthorizer {
 
@@ -20,64 +19,106 @@ export class WardenAuthorizer {
     return this.opts.storageProvider.findEntryById(userId);
   }
 
-  public async findDecoratorBy(userId: string): Promise<WardenUserDecoration<any>> {
+  public async findDecoratorById(userId: string): Promise<WardenUserDecoration<any>> {
     const user: WardenEntry = await this.findEntryById(userId);
     const rval: WardenUserDecoration<any> = user ? await this.opts.userDecorationProvider.fetchDecoration(user) : null;
     return rval;
   }
 
-  public async userHasGlobalRole(userId: string, roleId: string): Promise<boolean> {
-    return this.userHasGlobalRoles(userId, [roleId], true);
+
+  public async userHasGlobalRoleById(userId: string, roleId: string): Promise<boolean> {
+    return this.userHasGlobalRole(await this.findDecoratorById(userId), roleId);
   }
 
-  public async userHasRoleOnTeam(userId: string,teamId: string, roleId: string): Promise<boolean> {
-    return this.userHasRolesOnTeam(userId, teamId, [roleId], true);
+  public async userHasRoleOnTeamById(userId: string,teamId: string, roleId: string): Promise<boolean> {
+    return this.userHasRolesOnTeam(await this.findDecoratorById(userId), teamId, [roleId], true);
   }
 
-  public async userHasAtLeastOneGlobalRole(userId: string, roleIds: string[]): Promise<boolean> {
-    return this.userHasGlobalRoles(userId, roleIds, false);
+  public async userHasAtLeastOneGlobalRoleById(userId: string, roleIds: string[]): Promise<boolean> {
+    return this.userHasGlobalRoles(await this.findDecoratorById(userId), roleIds, false);
   }
 
-  public async userHasAtLeastOneRoleOnTeam(userId: string, teamId: string, roleIds: string[]): Promise<boolean> {
-    return this.userHasRolesOnTeam(userId, teamId, roleIds, false);
-  }
-
-
-  public async userHasAllGlobalRoles(userId: string, roleIds: string[]): Promise<boolean> {
-    return this.userHasGlobalRoles(userId, roleIds, true);
-  }
-
-  public async userHasAllRolesOnTeam(userId: string, teamId: string, roleIds: string[]): Promise<boolean> {
-    return this.userHasRolesOnTeam(userId, teamId, roleIds, true);
+  public async userHasAtLeastOneRoleOnTeamById(userId: string, teamId: string, roleIds: string[]): Promise<boolean> {
+    return this.userHasRolesOnTeam(await this.findDecoratorById(userId), teamId, roleIds, false);
   }
 
 
-  public async userHasGlobalRoles(userId: string, roleIds: string[], combineWithAnd: boolean): Promise<boolean> {
-    const user: WardenUserDecoration<any> = await this.findDecoratorBy(userId);
+  public async userHasAllGlobalRolesById(userId: string, roleIds: string[]): Promise<boolean> {
+    return this.userHasGlobalRoles(await this.findDecoratorById(userId), roleIds, true);
+  }
+
+  public async userHasAllRolesOnTeamById(userId: string, teamId: string, roleIds: string[]): Promise<boolean> {
+    return this.userHasRolesOnTeam(await this.findDecoratorById(userId), teamId, roleIds, true);
+  }
+
+
+  public async userHasGlobalRolesById(userId: string, roleIds: string[], combineWithAnd: boolean): Promise<boolean> {
+    return WardenUtils.userHasGlobalRoles(await this.findDecoratorById(userId), roleIds, combineWithAnd);
+  }
+
+  public async userHasRolesOnTeamById(userId: string, teamId: string, roleIds: string[], combineWithAnd: boolean): Promise<boolean> {
+    return WardenUtils.userHasRolesOnTeam(await this.findDecoratorById(userId), teamId, roleIds, combineWithAnd);
+  }
+
+  // Just a synonym since that is how some people think
+  public async userIsTeamMemberById(userId: string, teamId: string): Promise<boolean> {
+    return WardenUtils.userIsTeamMember(await this.findDecoratorById(userId),teamId);
+  }
+
+  public async userHasAnyRoleOnTeamById(userId: string, teamId: string): Promise<boolean> {
+    return WardenUtils.userHasAnyRoleOnTeam(await this.findDecoratorById(userId),teamId);
+  }
+
+  public async usersTeamMembershipsById(userId: string): Promise<string[]> {
+    return WardenUtils.usersTeamMemberships(await this.findDecoratorById(userId));
+  }
+
+
+  // Simple passthrus for convenience
+  public userHasGlobalRole(user: WardenUserDecoration<any>, roleId: string): boolean {
+    return WardenUtils.userHasGlobalRoles(user, [roleId], true);
+  }
+
+  public userHasRoleOnTeam(user: WardenUserDecoration<any>,teamId: string, roleId: string): boolean {
+    return WardenUtils.userHasRolesOnTeam(user, teamId, [roleId], true);
+  }
+
+  public userHasAtLeastOneGlobalRole(user: WardenUserDecoration<any>, roleIds: string[]): boolean {
+    return WardenUtils.userHasGlobalRoles(user, roleIds, false);
+  }
+
+  public userHasAtLeastOneRoleOnTeam(user: WardenUserDecoration<any>, teamId: string, roleIds: string[]): boolean {
+    return WardenUtils.userHasRolesOnTeam(user, teamId, roleIds, false);
+  }
+
+
+  public userHasAllGlobalRoles(user: WardenUserDecoration<any>, roleIds: string[]): boolean {
+    return WardenUtils.userHasGlobalRoles(user, roleIds, true);
+  }
+
+  public userHasAllRolesOnTeam(user: WardenUserDecoration<any>, teamId: string, roleIds: string[]): boolean {
+    return WardenUtils.userHasRolesOnTeam(user, teamId, roleIds, true);
+  }
+
+
+  public userHasGlobalRoles(user: WardenUserDecoration<any>, roleIds: string[], combineWithAnd: boolean): boolean {
     return WardenUtils.userHasGlobalRoles(user, roleIds, combineWithAnd);
   }
 
-  public async userHasRolesOnTeam(userId: string, teamId: string, roleIds: string[], combineWithAnd: boolean): Promise<boolean> {
-    const user: WardenUserDecoration<any> = await this.findDecoratorBy(userId);
+  public userHasRolesOnTeam(user: WardenUserDecoration<any>, teamId: string, roleIds: string[], combineWithAnd: boolean): boolean {
     return WardenUtils.userHasRolesOnTeam(user, teamId, roleIds, combineWithAnd);
   }
 
-
-
-
   // Just a synonym since that is how some people think
-  public async userIsTeamMember(userId: string, teamId: string): Promise<boolean> {
-    const user: WardenUserDecoration<any> = await this.findDecoratorBy(userId);
+  public userIsTeamMember(user: WardenUserDecoration<any>, teamId: string): boolean {
     return WardenUtils.userIsTeamMember(user,teamId);
   }
 
-  public async userHasAnyRoleOnTeam(userId: string, teamId: string): Promise<boolean> {
-    const user: WardenUserDecoration<any> = await this.findDecoratorBy(userId);
+  public userHasAnyRoleOnTeam(user: WardenUserDecoration<any>, teamId: string): boolean {
     return WardenUtils.userHasAnyRoleOnTeam(user,teamId);
   }
 
-  public async usersTeamMemberships(userId: string): Promise<string[]> {
-    const user: WardenUserDecoration<any> = await this.findDecoratorBy(userId);
+  public usersTeamMemberships(user: WardenUserDecoration<any>): string[] {
     return WardenUtils.usersTeamMemberships(user);
   }
 
