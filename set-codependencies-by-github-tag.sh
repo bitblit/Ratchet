527b3255fa476473a87bb24b8d0ee0ff5997e454
Christopher Weiss <bitblit@gmail.com>
2025-11-03 15:30:29 -0800

    Fix test

--- set-codependencies-by-github-tag.sh
+++ set-codependencies-by-github-tag.sh
@@ -3,16 +3,23 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+echo Github tag reported as $GITHUB_REF_NAME
+
 # Determine version based on GITHUB_REF_NAME
 VERSION="$1"
-if [[ "$GITHUB_REF_NAME" == release-* ]]; then
+
+case "$GITHUB_REF_NAME" in
+  release-*)
     echo "Release branch detected (${GITHUB_REF_NAME}), using version: ${VERSION}"
-elif [[ "$GITHUB_REF_NAME" == alpha-* ]]; then
+    ;;
+  alpha-*)
     VERSION="${1}-alpha"
     echo "Alpha branch detected (${GITHUB_REF_NAME}), using version: ${VERSION}"
-else
+    ;;
+  *)
     echo "Using version: ${VERSION}"
-fi
+    ;;
+esac
 
 echo "Applying version number to all package.json files: ${VERSION}"
 find . -regex "\./modules/[^/]*/package.json" -type f -exec sed -i -e "s/0.0.0-snapshot/${VERSION}/g" {} \;
