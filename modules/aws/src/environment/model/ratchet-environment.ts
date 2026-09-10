7c6a9facc6f65c1122717b733e9b209acb899e72
Christopher Weiss <bitblit@gmail.com>
2026-09-09 19:27:18 -0700

    Adding common third party

--- modules/aws/src/environment/model/ratchet-environment.ts
+++ modules/aws/src/environment/model/ratchet-environment.ts
@@ -1,11 +1,14 @@
 import { RatchetEnvironmentDynamicProperty } from "./ratchet-environment-dynamic-property.ts";
 import { RatchetEnvironmentGenericThirdPartyConfig } from "./ratchet-environment-generic-third-party-config.ts";
+import {
+  RatchetEnvironmentCommonThirdPartyConfig
+} from "./ratchet-environment-common-third-party-config.ts";
 
 export interface RatchetEnvironment {
   encryptionKey?: string;
   historicalDecryptionKeys?: string[];
   database?: RatchetEnvironmentDynamicProperty[];
 
-
+  commonThirdParty?: RatchetEnvironmentCommonThirdPartyConfig;
   otherThirdParty?: RatchetEnvironmentGenericThirdPartyConfig[];
 }
